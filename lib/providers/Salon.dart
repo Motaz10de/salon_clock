9a13714673dd573ff7d65ddcee74860958c7b0ec
Motaz10de <98272321+Motaz10de@users.noreply.github.com>
2022-04-10 04:25:08 +0300

    OIoii

--- lib/providers/Salon.dart
+++ lib/providers/Salon.dart
@@ -41,7 +41,7 @@ class Salon with ChangeNotifier {
     final url = Uri.parse(
         'https://test11-eb4c6-default-rtdb.firebaseio.com/userFavorites/$userId/$id.json?auth=$token');
     try {
-      final response = await http.put(url, body: json.encode(({isFavorite})));
+      final response = await http.put(url, body: json.encode(isFavorite));
       if (response.statusCode >= 400) {
         isFavorite = oldStatus;
         notifyListeners();
